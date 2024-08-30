4b6b168a7f80ca5cdd570624c2d377e93ebada97
Hossam. <66652255+HossamSadekk@users.noreply.github.com>
2024-08-30 19:53:31 +0300

    Handling loading & error state

--- app/src/main/java/com/example/moviedb/ui/movieList/fragment/MovieListFragment.kt
+++ app/src/main/java/com/example/moviedb/ui/movieList/fragment/MovieListFragment.kt
@@ -1,6 +1,10 @@
 package com.example.moviedb.ui.movieList.fragment
 
+import androidx.core.view.isVisible
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.paging.LoadState
 import com.example.moviedb.core.base.fragment.BaseFragment
 import com.example.moviedb.databinding.FragmentMovieListBinding
 import com.example.moviedb.ui.movieList.adapter.MovieAdapter
@@ -15,21 +19,56 @@ class MovieListFragment : BaseFragment<FragmentMovieListBinding, MovieListViewMo
         MovieAdapter(
             onClickShowDetails = {
 
+            }, onClickLike = {
+
             }
         )
     }
 
     override fun setupUI() {
-        binding.apply {
-            movieRecyclerView.adapter = movieAdapter
+        initRecyclerView()
+    }
+
+    private fun initRecyclerView() {
+        binding.movieRecyclerView.adapter = movieAdapter
+
+        movieAdapter.addLoadStateListener { loadState ->
+            when (val refreshState = loadState.refresh) {
+                is LoadState.Loading -> {
+                    viewModel.startLoading()
+                }
+
+                is LoadState.Error -> {
+                    val error = refreshState.error
+                    binding.tvError.isVisible = true
+                    binding.tvError.text = error.message
+                    viewModel.stopLoading()
+                }
+
+                else -> {
+                    viewModel.stopLoading()
+                }
+            }
         }
     }
 
     override fun observeViewModel() {
-        lifecycleScope.launch {
-            viewModel.movies.collectLatest { pagingData ->
-                movieAdapter.submitData(pagingData)
+        viewLifecycleOwner.lifecycleScope.launch {
+            // This block runs only when the lifecycle is in the STARTED state
+            viewLifecycleOwner.lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.movies.collectLatest { pagingData ->
+                    movieAdapter.submitData(pagingData)
+                }
             }
+        }
+
+        viewModel.loading.observe(viewLifecycleOwner) { isLoading ->
+            binding.progressCircular.isVisible = isLoading
+        }
+
+        viewModel.error.observe(viewLifecycleOwner) { error ->
+            binding.tvError.isVisible = true
+            binding.tvError.text = error
         }
     }
 
