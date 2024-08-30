b8d3aba57cb3db226221d1267788fa695978f0f1
Hossam. <66652255+HossamSadekk@users.noreply.github.com>
2024-08-30 13:43:32 +0300

    Adding application class

--- app/src/main/java/com/example/moviedb/acitivty/MainActivity.kt
+++ app/src/main/java/com/example/moviedb/acitivty/MainActivity.kt
@@ -1,17 +1,19 @@
-package com.example.moviedb
+package com.example.moviedb.acitivty
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.moviedb.R.id
+import com.example.moviedb.R.layout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        setContentView(layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
