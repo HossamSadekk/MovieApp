d2a3800fedd49c803c030249cec3993a700f7eef
Hossam. <66652255+HossamSadekk@users.noreply.github.com>
2024-08-30 13:28:47 +0300

    Adding Initial Files

---
+++ app/build.gradle.kts
@@ -0,0 +1,70 @@
+plugins {
+    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.jetbrainsKotlinAndroid)
+    alias(libs.plugins.hilt.plugin)
+    id("kotlin-kapt")
+    id("kotlin-parcelize")
+    alias(libs.plugins.ksp)
+}
+
+android {
+    namespace = "com.example.moviedb"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.moviedb"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    // DI
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.compiler)
+    // Network
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.gson)
+    implementation(libs.okhttp)
+    implementation(libs.okhttp.logging)
+    implementation(libs.gson)
+    // DB
+    implementation(libs.room.runtime)
+    ksp(libs.room.compiler)
+    implementation(libs.room.ktx)
+    // Logging
+    implementation(libs.timber)
+    // Pagination
+    implementation(libs.pagingRuntime)
+    testImplementation(libs.pagingCommon)
+    // Navigation
+    implementation(libs.navigationFragment)
+    implementation(libs.navigationUI)
+
+}
